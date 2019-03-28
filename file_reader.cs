01c974cf779548e56f37e433f22f1b46411ff25e
juanhlb <juanhlb@github.com>
2019-03-27 21:32:00 -0400

    Create file_reader.cs

---
+++ file_reader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace file_reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter ofile ;
+            ofile = new StreamWriter("test.txt");
+            ofile.WriteLine("this is a message");
+            ofile.WriteLine("Hello world!");
+
+            ofile.Close();
+
+
+            //read file
+            StreamReader ifile;
+            Console.WriteLine("this is the content of file test.txt: \n" );
+
+            ifile = new StreamReader("test.txt");
+            Console.WriteLine (ifile.ReadToEnd ());
+            ifile.Close();
+            Console.ReadKey();
+            
+
+        }
+    }
+}
